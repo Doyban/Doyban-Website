143816c6bd714889491905300054a350d16e2c4a
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-05 15:51:38 +0200

    feat: add dates to description of our story in About.vue

--- pages/About.vue
+++ pages/About.vue
@@ -104,7 +104,7 @@ export default Vue.extend({
       {
         color: 'green',
         description:
-          "Daniel Danielecki founds Doyban as a hobby project for his HTML5 games after his Engineer's Thesis 'The game application in HTML 5 technology designed for mobile devices'. The thesis project and the first game was based on Quintus, but all the next games have been shifted to Phaser.",
+          "Daniel Danielecki founds Doyban back in 2016 as a hobby project for his HTML5 games. After creating the first game within the scope of his Engineer's Thesis, 'The game application in HTML 5 technology designed for mobile devices'. The thesis' project and the first game were based on Quintus, but all the next games have been shifted to Phaser.",
         icon: 'mdi-rocket-launch-outline',
         title: 'Doyban has been founded',
         year: 2016,
@@ -112,7 +112,7 @@ export default Vue.extend({
       {
         color: 'orange',
         description:
-          "In 2015, Daniel read in a book (Luke Stevens, RJ Owen: 'The Truth About HTML5, Apress', 2012) about futuristic use cases for HTML5 technology. In 2019, Facebook came up with Messenger Instant Games, a breakthrough for Doyban, quickly increasing the number of players above 100.000, making this hobby project more aspiring.",
+          "In 2015, Daniel read in a book (Luke Stevens, RJ Owen: 'The Truth About HTML5, Apress', 2012) about futuristic use cases for HTML5 technology while preparing for his thesis. In 2019, Facebook came up with Messenger Instant Games, a breakthrough for Doyban, quickly increasing the number of players above 100.000, making this hobby project more aspiring.",
         icon: 'mdi-lightbulb-on-outline',
         title: 'Instant Games',
         year: 2019,
@@ -120,7 +120,7 @@ export default Vue.extend({
       {
         color: 'deep-purple',
         description:
-          'We have launched a crowdfunding campaign on the world-famous Kickstarter platform. The campaign was about Mobile Games without Installation, Mini Games on popular apps and chats like Facebook Gaming, iMessage, Messenger, Snapchat, Telegram, or WeChat. It was featured widely in Australia, Belgium, Ecuador, Germany, India, Japan, Poland, United Kingdom (UK), United States (USA) by many blogs, gaming portals, technological news, websites, etc., such as Gamasutra. Unfortunately, we have not been successfully funded. There might be several reasons: audience did not what we wanted to achieve, mobiles gamers do not follow Kickstarter, too early product stage, or just a bad idea.',
+          'We have launched a crowdfunding campaign on the world-famous Kickstarter platform on May 12, 2021. The campaign was about Mobile Games without Installation, Mini Games on popular apps and chats like Facebook Gaming, iMessage, Messenger, Snapchat, Telegram, or WeChat. It was featured widely in Australia, Belgium, Ecuador, Germany, India, Japan, Poland, United Kingdom (UK), United States (USA) by many blogs, gaming portals, technological news, websites, etc., such as Gamasutra. Unfortunately, we have not been successfully funded. There might be several reasons: the audience did not what we wanted to achieve, mobiles gamers do not follow Kickstarter, too early product stage, or just a bad idea.',
         icon: 'mdi-kickstarter',
         title: 'Kickstarter: #5G Ready, Mobile Games.',
         year: 2021,
@@ -128,7 +128,7 @@ export default Vue.extend({
       {
         color: 'brown',
         description:
-          'We believe Mobile Games without Installation on platforms such as Facebook Gaming, iMessage, Messenger, Snapchat, or Telegram will evolve. However, in the meantime, we continue publishing our games as native mobile applications on Android and iOS. All our games are installation less ready once our desired ecosystem evolves.',
+          'We believe Mobile Games without Installation on platforms such as Facebook Gaming, iMessage, Messenger, Snapchat, or Telegram will evolve. However, in the meantime, from 2022, we continue publishing our games as native mobile applications on Android and iOS. All our games are installation less ready once our desired ecosystem evolves.',
         icon: 'mdi-cellphone-arrow-down',
         title: 'Mobile Games',
         year: 2022,
