b18a65070b22f950b0b93656ee005837dcadc802
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-05 14:30:04 +0200

    revert: delete 1 parallax section in Home

--- pages/index.vue
+++ pages/index.vue
@@ -10,32 +10,6 @@
         </div>
       </v-col>
     </v-row>
-    <v-parallax :src="require('@/static/pages/parallax1.jpeg')" height="200">
-      <h1 class="display-3 text-center white--text">Hello</h1>
-      <h2 class="text-center">Random text bla bla</h2>
-    </v-parallax>
-    <v-row justify="center">
-      <v-col sm="4" xs="12">
-        <h2 class="text-center">Some slogan</h2>
-        <p class="text-center">Paragraph goes here</p>
-        <v-divider />
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col v-for="(point, index) in points" :key="index" md="4" xs="12">
-        <v-card class="transparent" flat>
-          <v-card-text class="text-center">
-            <v-icon color="purple darken-4" x-large>{{ point.icon }}</v-icon>
-          </v-card-text>
-          <v-card-title class="justify-center">
-            <h3>{{ point.title }}</h3>
-          </v-card-title>
-          <v-card-text>
-            <p>{{ point.description }}</p>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </v-row>
     <v-parallax :src="require('@/static/pages/parallax2.jpeg')" height="200">
       <h2 class="display-2 text-center">Random text bla bla</h2>
       <em class="text-center">Super super text goes here.</em>
