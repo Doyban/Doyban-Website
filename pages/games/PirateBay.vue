dffec0cecaa88ec7049c1941f3393663c63eb346
Daniel Danielecki <daniel.danielecki@mail.com>
2021-10-03 15:37:27 +0200

    feat: add temporarly solution for games image gallery

--- pages/games/PirateBay.vue
+++ pages/games/PirateBay.vue
@@ -1,5 +1,27 @@
 <template>
   <v-row>
+    <v-col
+      v-for="item in items"
+      :key="item.id"
+      class="d-flex child-flex"
+      cols="4"
+    >
+      <v-img
+        :src="item.src"
+        contain
+        class="grey lighten-2"
+        @click="selectedItem = item"
+      >
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-col>
     <v-col class="text-center">
       <img src="/v.png" alt="Vuetify.js" class="mb-5" />
       <blockquote class="blockquote">
@@ -10,6 +32,30 @@
           </small>
         </footer>
       </blockquote>
+      <v-overlay v-if="selectedItem">
+        <v-img
+          :src="selectedItem ? selectedItem.src : ''"
+          contain
+          @click="selectedItem = null"
+        ></v-img>
+      </v-overlay>
     </v-col>
   </v-row>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      selectedItem: null,
+      items: [
+        { id: 1, src: require('@/static/pages/about.png') },
+        { id: 2, src: require('@/static/pages/about.png') },
+        { id: 3, src: require('@/static/pages/about.png') },
+      ],
+    }
+  },
+})
+</script>
