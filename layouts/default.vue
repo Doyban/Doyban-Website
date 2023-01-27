cc8648a9765265d3518fdf1412d23880cc2d2dd7
Daniel Danielecki <daniel.danielecki@mail.com>
2023-01-27 17:10:26 +0100

    docs: remove 1 TODO in default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,5 @@
 <template>
   <!-- TODO: Add meta tags. -->
-  <!-- TODO: Migrate to Vue.js 3 once Nuxt.js 3 will be release. -->
   <!-- TODO: Change PWA icons. -->
   <!-- TODO: Go through all the app's template/Vuetify elements/code. -->
   <v-app>
