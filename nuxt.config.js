a799423319cb96593b6edc45ce8ea446f045bc28
Daniel Danielecki <daniel.danielecki@mail.com>
2023-02-25 15:55:42 +0100

    feat: add meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,293 @@ require('dotenv').config()
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    titleTemplate: '%s - doyban',
-    title: 'doyban',
+    titleTemplate: '%s - Doyban | The World of Digital Gameplay.',
+    title: 'Doyban',
     meta: [
+      // Basic HTML meta tags.
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'author', name: 'author', content: 'Doyban, contact@doyban.com' },
+      { hid: 'copyright', name: 'copyright', content: 'Doyban' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          '🚀 Mobile Games without Installation are Mini Games on popular apps and chats like Facebook Gaming, iMessage, Messenger, Snapchat, Telegram, or WeChat.',
+      },
+      { hid: 'language', name: 'language', content: 'en_US' },
+      { hid: 'robots', name: 'robots', content: 'index,follow' },
+      {
+        hid: 'viewport',
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+
+      // Apple meta tags.
+      {
+        hid: 'apple-mobile-web-app-capable',
+        name: 'apple-mobile-web-app-capable',
+        content: 'yes',
+      },
+      {
+        hid: 'apple-mobile-web-app-status-bar-style',
+        name: 'apple-mobile-web-app-status-bar-style',
+        content: 'black',
+      },
+      {
+        hid: 'format-detection',
+        name: 'format-detection',
+        content: 'telephone=true',
+      },
+
+      // Open Graph / Facebook meta tags.
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content:
+          '🚀 Mobile Games without Installation are Mini Games on popular apps and chats like Facebook Gaming, iMessage, Messenger, Snapchat, Telegram, or WeChat.',
+      },
+      { hid: 'og:image', property: 'og:image', content: '/logo.webp' },
+      {
+        hid: 'og:image:alt',
+        property: 'og:image:alt',
+        content: 'This is logo of Doyban',
+      },
+      { hid: 'og:locale', property: 'og:locale', content: 'en_US' },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ar',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'be_BY',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'bg_BG',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'cs_CZ',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'da_DK',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'de_DE',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'el_GR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'es_ES',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'et_EE',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'fi_FI',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'fr_FR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'he_IL',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'hi_IN',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'hr_HR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'hu_HU',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'id_ID',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'is_IS',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'it_IT',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ja_JP',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ko_KR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'nb_NO',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'nl_NL',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'pl_PL',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'pt_PT',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ro_RO',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ru_RU',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'sk_SK',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'sr_SR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'sv_SE',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ta',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'te',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'th_TH',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'tr_TR',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'uk_UA',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'ur',
+      },
+      {
+        hid: 'og:locale:alternate',
+        property: 'og:locale:alternate',
+        content: 'zh_TW',
+      },
+      { hid: 'og:site_name', property: 'og:site_name', content: 'Doyban' },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Doyban | The World of Digital Gameplay.',
+      },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      { hid: 'og:url', property: 'og:url', content: 'https://doyban.com' },
+
+      // Twitter meta tags.
+      {
+        hid: 'twitter:card',
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        hid: 'twitter:creator',
+        property: 'twitter:creator',
+        content: '@ddanielecki',
+      },
+      {
+        hid: 'twitter:description',
+        property: 'twitter:description',
+        content:
+          '🚀 Mobile Games without Installation are Mini Games on popular apps and chats like Facebook Gaming, iMessage, Messenger, Snapchat, Telegram, or WeChat.',
+      },
+      {
+        hid: 'twitter:image',
+        property: 'twitter:image',
+        content: '/logo.webp',
+      },
+      {
+        hid: 'twitter:image:alt',
+        property: 'twitter:image:alt',
+        content: 'This is logo of Doyban',
+      },
+      {
+        hid: 'twitter:site',
+        property: 'twitter:site',
+        content: '@doybangames',
+      },
+      {
+        hid: 'twitter:title',
+        property: 'twitter:title',
+        content: 'Doyban | The World of Digital Gameplay.',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'shortcut icon', type: 'image/ico', href: '/favicon.ico' },
+    ],
+    htmlAttrs: {
+      lang: 'en',
+      amp: true,
+    },
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
