173b7bb794fd90d9defc8f9583dd42239899453f
Daniel Danielecki <daniel.danielecki@mail.com>
2023-09-18 17:33:54 +0200

    🔧 chore(vuetify.ts): comment out theme configuration to be implemented later

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -5,6 +5,9 @@ import { createVuetify } from 'vuetify'
 export default defineNuxtPlugin(nuxtApp => {
   const vuetify = createVuetify({
     ssr: true,
+    // theme: {
+    //   defaultTheme: 'dark' // TODO: Create toggle to switch between light/dark theme and it's easy to do it from here.
+    // }
   })
 
   nuxtApp.vueApp.use(vuetify)
