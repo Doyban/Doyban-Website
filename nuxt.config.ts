f743fb1139e6217c977c7288cafc5c4f5a5d6c72
Daniel Danielecki <daniel.danielecki@mail.com>
2023-09-29 11:16:06 +0200

    🔥 refactor(nuxt.config.ts): remove unused import and commented out code related to Vuetify configuration
    🔧 chore(nuxt.config.ts): remove unused Vuetify module configuration and commented out code related to custom SCSS variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,3 @@
-// import colors from 'vuetify/es5/util/colors'
 import vuetify from 'vite-plugin-vuetify'
 
 // PWA Config
@@ -439,24 +438,4 @@ export default defineNuxtConfig({
     navigateFallback: '/',
     globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
   },
-
-  // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
-  // TODO: Use variables for SCSS.
-  // vuetify: {
-  //   customVariables: ['~/assets/variables.scss'],
-  //   theme: {
-  //     dark: false,
-  //     themes: {
-  //       dark: {
-  //         primary: colors.blue.darken2,
-  //         accent: colors.grey.darken3,
-  //         secondary: colors.amber.darken3,
-  //         info: colors.teal.lighten1,
-  //         warning: colors.amber.base,
-  //         error: colors.deepOrange.accent4,
-  //         success: colors.green.accent3,
-  //       },
-  //     },
-  //   },
-  // },
 })
